c2d9cb98e307f5915b9efebeb3df9c537b4b9b44
Yabsra <yabsra23@gmail.com>
2022-06-01 00:15:32 +0300

    feat: add user input validation

--- components/AddUser.jsx
+++ components/AddUser.jsx
@@ -9,25 +9,96 @@ const AddUser = ({ setClose }) => {
     const [username, setUsername] = useState(null);
     const [password, setPassword] = useState(null);
     const [email, setEmail] = useState(null);
+    const [error, setError] = useState(false);
 
 
     const handleCreate = async () => {
-        console.log('add called')
+        setError(validate());
         try {
-            const newUser = {
-                name,
-                username,
-                password,
-                email,
-            };
+            if (!error) {
+                const newUser = {
+                    name,
+                    username,
+                    password,
+                    email,
+                };
 
-            await axios.post("http://localhost:3000/api/users", newUser);
-            setClose(true);
-            window.location.reload();
+                await axios.post("http://localhost:3000/api/users", newUser);
+                setClose(true);
+                window.location.reload();
+            }
         } catch (err) {
             console.error(err);
         }
     };
+
+    const validate = () => {
+        const nameValidation = validateName();
+        if (nameValidation) {
+            return nameValidation;
+        }
+
+        const usernameValidation = validateUsername();
+        if (usernameValidation) {
+            return usernameValidation;
+        }
+
+        const passwordValidation = validatePassword();
+        if (passwordValidation) {
+            return passwordValidation;
+        }
+
+        const emailValidation = validateEmail();
+        if (emailValidation) {
+            return emailValidation;
+        }
+    }
+
+    const validateName = () => {
+        if (name === null) {
+            return 'Name is required'
+        }
+
+        if (name.length < 3) {
+            return 'Name must be at least 3 characters'
+        }
+    }
+
+    const validateUsername = () => {
+        if (username === null) {
+            return 'Username is required'
+        }
+
+        if (username.length < 3) {
+            return 'Username must be at least 3 characters'
+        }
+    }
+
+    const validateEmail = () => {
+        if (email === null) {
+            return 'Email is required'
+        }
+        const emailRegEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        if (!emailRegEx.test(email)) {
+            return 'Email is not valid'
+        }
+    }
+
+
+    const validatePassword = () => {
+        if (password === null) {
+            return 'Password is required'
+        }
+
+        if (password.length < 8) {
+            return 'Password must be at least 8 characters'
+        }
+
+        if (password.length > 16) {
+            return 'Password must be less than 16 characters'
+        }
+    }
+
 
     return (
         <div className={styles.container}>
@@ -42,6 +113,7 @@ const AddUser = ({ setClose }) => {
                         className={styles.input}
                         type="text"
                         onChange={(e) => setName(e.target.value)}
+                        data-cy="sign-up-name"
                     />
                 </div>
                 <div className={styles.item}>
@@ -50,6 +122,7 @@ const AddUser = ({ setClose }) => {
                         className={styles.input}
                         type="text"
                         onChange={(e) => setUsername(e.target.value)}
+                        data-cy="sign-up-username"
                     />
                 </div>
                 <div className={styles.item}>
@@ -58,6 +131,7 @@ const AddUser = ({ setClose }) => {
                         className={styles.input}
                         type="text"
                         onChange={(e) => setEmail(e.target.value)}
+                        data-cy="sign-up-email"
                     />
                 </div>
                 <div className={styles.item}>
@@ -66,9 +140,14 @@ const AddUser = ({ setClose }) => {
                         className={styles.input}
                         type="password"
                         onChange={(e) => setPassword(e.target.value)}
+                        data-cy="sign-up-password"
 
                     />
                 </div>
+                {
+                    error ?
+                        <p>{error}</p> : null
+                }
                 <button className={styles.addButton} onClick={handleCreate}>
                     Create
                 </button>
